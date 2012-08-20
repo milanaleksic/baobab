47f9601cd9a0ebe5f518175b50640ff807b01655
Milan Aleksic <milanaleksic@gmail.com>
2012-08-20 10:35:24 +0200

    nope, no abstract Model class will be used

--- swt-gui-transformer-core/src/main/java/net/milanaleksic/guitransformer/model/Model.java
+++
@@ -1,9 +0,0 @@
-package net.milanaleksic.guitransformer.model;
-
-/**
- * User: Milan Aleksic
- * Date: 8/7/12
- * Time: 2:48 PM
- */
-public abstract class Model {
-}
--- swt-gui-transformer-core/src/test/java/net/milanaleksic/guitransformer/ModelAcceptanceTestModel.java
+++ swt-gui-transformer-core/src/test/java/net/milanaleksic/guitransformer/ModelAcceptanceTestModel.java
@@ -1,13 +1,11 @@
 package net.milanaleksic.guitransformer;
 
-import net.milanaleksic.guitransformer.model.Model;
-
 /**
  * User: Milan Aleksic
  * Date: 8/7/12
  * Time: 2:14 PM
  */
-public class ModelAcceptanceTestModel extends Model {
+public class ModelAcceptanceTestModel {
 
     private String text1;
 
