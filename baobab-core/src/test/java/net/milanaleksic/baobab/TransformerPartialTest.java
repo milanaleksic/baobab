19101151516f6b94797b6af1bcc6092f9e2e01a7
Milan Aleksic <milanaleksic@gmail.com>
2014-03-02 21:37:59 +0100

    closes #35 (Editor should handle GUI files without shell root control)

---
+++ baobab-core/src/test/java/net/milanaleksic/baobab/TransformerPartialTest.java
@@ -0,0 +1,29 @@
+package net.milanaleksic.baobab;
+
+import net.milanaleksic.baobab.test.GuiceRunner;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Shell;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+
+/**
+ * User: Milan Aleksic (milanaleksic@gmail.com)
+ * Date: 02/03/2014
+ */
+@RunWith(GuiceRunner.class)
+public class TransformerPartialTest {
+
+    @Inject
+    private Transformer transformer;
+
+    @Test
+    public void partial_transform() {
+        Shell parentShellForPartial = new Shell(SWT.DIALOG_TRIM);
+        transformer.createFormFromResource("/net/milanaleksic/baobab/TransformerPartialTest.gui", parentShellForPartial, null);
+        parentShellForPartial.dispose();
+    }
+
+
+}
